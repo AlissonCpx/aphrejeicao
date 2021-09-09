f5617baba98d8f3438ae1f7a5debc8181ced3cb0
AlissonCpx <alissondeandrade1@gmail.com>
2021-09-09 00:58:41 -0300

    Alteração do computador para o mac

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/telas/home.dart';
+import 'package:myapp/telas/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: Login(),
   ));
 }
--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,16 +1,21 @@
 //import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/entidades/usuario.dart';
 
 class Util {
   static Future<Usuario> login(String login, String senha) async {
 
 
-  //  CollectionReference users = FirebaseFirestore.instance.collection('usuarios');
-
-    //print(users.id);
-
+    CollectionReference users = await FirebaseFirestore.instance.collection('usuarios');
+    List<DocumentSnapshot> userFb = (await users.where("login", isEqualTo: login).where("senha", isEqualTo: senha).get()).docs;
     Usuario user = new Usuario();
-
+    if (userFb.isNotEmpty) {
+      user.nome = userFb[0]["nome"];
+      user.login = userFb[0]["login"];
+      user.senha = userFb[0]["senha"];
+      user.admin = userFb[0]["admin"];
+    }
     return user;
   }
 }
